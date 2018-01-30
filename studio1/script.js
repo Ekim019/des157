dec9e6ac48c0a8f2487c2068d238f43ce7fcf5c4
eunice kim <eudkim@ucdavis.edu>
2018-01-29 20:47:24 -0800

    fix the show and hide
    fix the show and hide the image and text.

--- studio1/script.js
+++ studio1/script.js
@@ -2,63 +2,63 @@
 //global elements
 console.log('reading');
 
-var results=document.getElementById("results");
-var myMsg=document.getElementById("myMsg");
+var results = document.getElementById("results");
+var myMsg = document.getElementById("myMsg");
 var userName = document.f.userName.value;
 var userAdj = document.f.userAdj.value;
 var userNoun = document.f.userNoun.value;
 var userBff = document.f.userBff.value;
-var userColor=document.f.userColor.value;
+var userColor = document.f.userColor.value;
 document.f.onsubmit = processForm;
-document.f.onreset=resetForm;
-
+document.f.onreset = resetForm;
+// fuction to set the message and to set the show.
 function processForm() {
+  myMsg.setAttribute('class', 'show')
   results.setAttribute('class', 'show');
 
   var userName = document.f.userName.value;
   var userAdj = document.f.userAdj.value;
   var userNoun = document.f.userNoun.value;
   var userBff = document.f.userBff.value;
-  var userColor=document.f.userColor.value;
+  var userColor = document.f.userColor.value;
 
+  // use the if condition for the if user type hannah the skelleton image comes out.
+  if (userBff === "Hannah") {
+    myMsg.innerHTML = "Today, I am going to ask " + userName + " to come to my house." + "<br> she/he sits next to me." +
+      " Everytime, I say 'hello'to her/him. she/he is just ignore me. I want to be close with " + userName +
+      "she/he wears the same " + userColor + " t-shirts with jeans every single day." + "when I ask " + userAdj +
+      "myfriend " + userBff + " why is " + userName + " so quite? " + userBff + " looks at me strangerly and " + userBff +
+      " <em>tell me," + userName + "died last year!</em>! I point to where" + userName + "sits, there is nobody" +
+      "I go home afterschool. At the door, someone is knocking in the front of my house. I shout'who is it?!', no one answers.I turn around." +
+      " someone keeps knocking on my door. I get frustrated. I open the door. " + userName + " is there.... " + userName +
+      " says,<em>'Hello! trick....A....Treat....BOO!!'</em>" + " I pass out in front of the door.I wake up on mybed. I realize it is Halloween and " +
+      userName + " cameback that day.";
 
-if (userBff === "Hannah") {
-  myMsg.innerHTML = "Today, I am going to ask " + userName + " to come to my house." + "<br> she/he sits next to me."
-  +" Everytime, I say 'hello'to her/him. she/he is just ignore me. I want to be close with "+userName+
-  "she/he wears the same "+userColor+" t-shirts with jeans every single day."+"when I ask "+userAdj+
-  "myfriend "+userBff+" why is "+userName+" so quite? "+userBff+" looks at me strangerly and "+userBff+
-  " <em>tell me,"+userName+"died last year!</em>! I point to where"+userName+"sits, there is nobody"+
-  "I go home afterschool. At the door, someone is knocking in the front of my house. I shout'who is it?!', no one answers.I turn around."+
-  " someone keeps knocking on my door. I get frustrated. I open the door. "+userName+" is there.... "+userName+
-  " says,<em>'Hello! trick....A....Treat....BOO!!'</em>"+" I pass out in front of the door.I wake up on mybed. I realize it is Halloween and "+
-  userName+" cameback that day.";
+    image.setAttribute("src", "../images/Hannah.png");
+    image.setAttribute("width", "300");
+    image.setAttribute("height", "300");
+    image.setAttribute("alt", "hannah")
 
-image.setAttribute("src","../images/Hannah.png");
-image.setAttribute("width","300");
-image.setAttribute("height","300");
-image.setAttribute("alt","hannah")
-  // return false;
-} else {
+  } else { //else the tomato vampire image come out with the text.
 
-  myMsg.innerHTML = "Today, I am going to ask " + userName + " to come to my house." + "<br> she/he sits next to me."
-  +" Everytime, I say 'hello'to her/him. she/he is just ignore me. I want to be close with "+userName+
-  "she/he wears the same "+userColor+" t-shirts with jeans every single day."+"when I ask "+userAdj+
-  "myfriend "+userBff+" why is "+userName+" so quite? "+userBff+" looks at me strangerly and "+userBff+
-  " <em>tell me,"+userName+"died last year!</em>! I point to where"+userName+"sits, there is nobody"+
-  "I go home afterschool. At the door, someone is knocking in the front of my house. I shout'who is it?!', no one answers.I turn around."+
-  " someone keeps knocking on my door. I get frustrated. I open the door. "+userName+" is there.... "+userName+
-  " says,<em>'Hello! trick....A....Treat....BOO!!'</em>"+" I pass out in front of the door.I wake up on mybed. I realize it is Halloween and "+
-  userName+" cameback that day.";
-  // return false;
-    myMsg.setAttribute('class','hide');
-}
+    myMsg.innerHTML = "Today, I am going to ask " + userName + " to come to my house." + "<br> she/he sits next to me." +
+      " Everytime, I say 'hello'to her/him. she/he is just ignore me. I want to be close with " + userName +
+      "she/he wears the same " + userColor + " t-shirts with jeans every single day." + "when I ask " + userAdj +
+      "myfriend " + userBff + " why is " + userName + " so quite? " + userBff + " looks at me strangerly and " + userBff +
+      " <em>tell me," + userName + "died last year!</em>! I point to where" + userName + "sits, there is nobody" +
+      "I go home afterschool. At the door, someone is knocking in the front of my house. I shout'who is it?!', no one answers.I turn around." +
+      " someone keeps knocking on my door. I get frustrated. I open the door. " + userName + " is there.... " + userName +
+      " says,<em>'Hello! trick....A....Treat....BOO!!'</em>" + " I pass out in front of the door.I wake up on mybed. I realize it is Halloween and " +
+      userName + " cameback that day.";
+
+  }
   return false;
 
 }
-
-function resetForm(){
-  myMsg.innerHTML="";
-  myMsg.setAttribute('class','hide');
-  results.setAttribute('class','hide');
+// set the hide the message and the image.
+function resetForm() {
+  myMsg.innerHTML = "";
+  myMsg.setAttribute('class', 'hide');
+  results.setAttribute('class', 'hide');
 
 }
