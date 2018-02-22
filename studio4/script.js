663e3e82e9f90c9bf4df1e412171f905a6ca103e
eunice kim <eudkim@ucdavis.edu>
2018-02-21 18:01:56 -0800

    fix the container
    fix the image that in side of the containre width and height.

--- studio4/script.js
+++ studio4/script.js
@@ -4,80 +4,90 @@
 console.log("reading js");
 
 
-var mygame=document.getElementById('mygame');
-var container=document.getElementById('container');
-var pixel=document.getElementById('pixel');
-// var pixel = document.querySelector("#pixel");
-var pixelW = pixel.offsetWidth; 
+var mygame = document.querySelector('#mygame');
+
+// var pixel = document.getElementById('pixel');
+var pixel = document.querySelector('#pixel');
+var pixelW = pixel.offsetWidth;
 var pixelH = pixel.offsetHeight;
 
-// var container=document.quarySlector('#container');
-// var containerW = container.offsetWidth; 
-// var containerH = container.offsetHeight;
+var container = document.querySelector('#container');
+var containerW = 540;
+var containerH = 540;
 
-var mygameLeft=0;
-var mygameTop=0;
+
+
+var mygameLeft = 0;
+var mygameTop = 0;
 
 var score = document.querySelector('#score');
 var scoreCount = 0;
 
 var pixelInterval = setInterval(moveDiv, 1000);
 
-pixel.addEventListener("e.keyCode", updateScore);
+// pixel.addEventListener("e.keyCode", updateScore);
+mygame.addEventListener("e.keyCode", updateScore);
 
 function moveDiv() {
-  // get random values for new position with 'px' for the css
-  // The Math.random() method generates a random number between 0 and almost 1
-  // The Math.floor() method rounds a number downwards to the nearest integer
+
   var newLeft = Math.floor(Math.random() * (containerW - pixelW)) + "px";
-  var newTop = Math.floor(Math.random() * (containernH - pixelH)) + "px";
-  
+  var newTop = Math.floor(Math.random() * (containerH - pixelH)) + "px";
+
 
   // update pixel location
   pixel.style.left = newLeft;
   pixel.style.top = newTop;
- 
-}
 
-function anim(e){
+}
+console.log(moveDiv);
+
+function anim(e) {
+  clearInterval(pixelInterval);
   //alert(e.keyCode) to figure it out the number of key pressed of the arrow(up,down,left,right)
-// left keypress
-  if(e.keyCode===39){
-mygameLeft+=20;
-mygame.style.left=mygameLeft+'px';
-  } 
-  // right keypress
-  if(e.keyCode===37){
-    mygameLeft-=20;
-
-    if(mygameLeft>=600){
-      mygameLeft-=20;
+  // left keypress
+  if (e.keyCode === 39) {
+    mygameLeft += 20;
+    mygame.style.left = mygameLeft + 'px';
+    if (mygameLeft >= containerW) {
+      mygameLeft -= 20;
     }
-mygame.style.left=mygameLeft+'px';
-if(mygameLeft<=0){
-  mygameLeft+=20;
-}
- // down keypress
-  } if(e.keyCode===40){
-    mygameTop+=20;
-    mygame.style.top=mygameTop+'px';
-      }
-       // up keypress
-   if(e.keyCode===38){
-      mygameTop-=20;
-      mygame.style.top=mygameTop+'px';
-      
-        }
+  }
+  // right keypress
+  if (e.keyCode === 37) {
+    mygameLeft -= 20;
+
+    // if(mygameLeft>=500){
+    //   mygameLeft-=20;
+    // }
+    mygame.style.left = mygameLeft + 'px';
+    if (mygameLeft <= 0) {
+      mygameLeft += 20;
+    }
+    // down keypress
+  }
+  if (e.keyCode === 40) {
+    mygameTop += 20;
+    mygame.style.top = mygameTop + 'px';
+
+    if (mygameTop >= containerH) {
+      mygameTop -= 20;
+    }
+  }
+  // up keypress
+  if (e.keyCode === 38) {
+    mygameTop -= 20;
+
+    mygame.style.top = mygameTop + 'px';
+    if (mygameTop <= 0) {
+      mygameTop += 20;
+    }
+  }
+  pixel.addEventListener("e.keyCode", updateScore);
 }
 
-document.onkeydown =anim;
+document.onkeydown = anim;
 
-function updateScore(){
+function updateScore() {
   scoreCount++;
-  score.innerHTML="Score:"+
-  scoreCount;
+  score.innerHTML = "Score:" + scoreCount;
 }
-
-
-
-
