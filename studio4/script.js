0453b73ba763f3b6706511de3d9d8553875331c5
eunice kim <eudkim@ucdavis.edu>
2018-02-21 23:01:40 -0800

    add new project
    add new project.

--- studio4/script.js
+++ studio4/script.js
@@ -4,45 +4,48 @@
 console.log("reading js");
 
 
-var mygame = document.querySelector('#mygame');
+var mygame = document.querySelector("#mygame");
 
 // var pixel = document.getElementById('pixel');
-var pixel = document.querySelector('#pixel');
+var pixel = document.querySelector("#pixel");
 var pixelW = pixel.offsetWidth;
 var pixelH = pixel.offsetHeight;
 
-var container = document.querySelector('#container');
+var container = document.querySelector("#container");
 var containerW = 540;
 var containerH = 540;
 
+var newLeft;
+var newTop;
 
 
-var mygameLeft = 0;
-var mygameTop = 0;
+var mygameLeft=0;
+var mygameTop=0;
 
-var score = document.querySelector('#score');
+var score = document.querySelector("#score");
 var scoreCount = 0;
+// var reset=document.querySelector("#stop");
 
-var pixelInterval = setInterval(moveDiv, 1000);
+var pixelInterval = setInterval(moveDiv, 3000);
 
 // pixel.addEventListener("e.keyCode", updateScore);
-mygame.addEventListener("e.keyCode", updateScore);
+// mygame.addEventListener("e.keyCode", updateScore);
 
 function moveDiv() {
-
-  var newLeft = Math.floor(Math.random() * (containerW - pixelW)) + "px";
-  var newTop = Math.floor(Math.random() * (containerH - pixelH)) + "px";
-
+  newLeft = Math.floor(Math.random() * (containerW - pixelW));
+  newTop = Math.floor(Math.random() * (containerH - pixelH));
 
   // update pixel location
-  pixel.style.left = newLeft;
-  pixel.style.top = newTop;
+  pixel.style.left = newLeft + "px";
+  pixel.style.top = newTop + "px";
 
+  updateScore();
+  // resetmyGame();
 }
 console.log(moveDiv);
 
 function anim(e) {
-  clearInterval(pixelInterval);
+
   //alert(e.keyCode) to figure it out the number of key pressed of the arrow(up,down,left,right)
   // left keypress
   if (e.keyCode === 39) {
@@ -82,12 +85,34 @@ function anim(e) {
       mygameTop += 20;
     }
   }
-  pixel.addEventListener("e.keyCode", updateScore);
-}
+  updateScore();
+// resetmyGame();
 
+}
 document.onkeydown = anim;
 
+
+
 function updateScore() {
-  scoreCount++;
-  score.innerHTML = "Score:" + scoreCount;
+
+  console.log(mygameLeft, mygameTop, newLeft, newTop);
+  // if(mygameLeft mygameTop newLeft newTop) {
+  if(Math.abs(mygameLeft- newLeft) <5) {
+    scoreCount++;
+    score.innerHTML = "Score:" + scoreCount;
+  }else if (Math.abs(mygameTop-newTop)<5){
+    scoreCount++;
+    score.innerHTML = "Score:" + scoreCount;
+  }
+
 }
+// function resetmyGame(){
+//
+//
+//   scoreCount=0;
+//   score.innerHTML="Score:"+scoreCount;
+//
+//   pixel.style.display='block';
+//
+//   pixelInterval=setInterval(moveDiv,3000);
+// }
